87bfc17bbb51222dda08a207144a50f00f57676d
haoshuai <haoshuai9192@gmail.com>
2020-03-19 22:38:29 +0800

    爬虫基础实现 实现一个图片下载的爬虫

---
+++ tutorial/spiders/doubannv.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from tutorial.items import TutorialItem
+
+class DoubannvSpider(scrapy.Spider):
+    name = 'doubannv'
+    allowed_domains = ['doubannv.net']
+    start_urls = ['http://www.doubannv.net/html/doubanfuli/index_2.html',
+    'http://www.doubannv.net/html/doubanfuli/index_3.html']
+
+    def __init__(self, category=None, *args, **kwargs):
+        pass
+
+    def start_requests(self):
+        for url in self.start_urls:
+            print("请求地址", url)
+            yield scrapy.Request(url,self.parse)
+        pass
+
+    def parse(self, response):
+        # '//img[@class="ui avatar image"]/@src'
+        title_list = response.xpath('//img[@class="ui avatar image"]/@alt').getall()
+        img_list = response.xpath('//img[@class="rounded ui  left image "]/@src').getall()
+        img_index_list = response.xpath('//a[@class="imga"]/@href').getall()
+
+        if len(title_list) == len(img_index_list) == len(img_list):
+            for i in range(len(title_list)): #0..<len(title_list):
+                item_title = title_list[i]
+                item_img = img_list[i]
+                item_index = img_index_list[i]
+                item = TutorialItem(title=item_title,img_url=item_img,index_url=item_index)
+                print("数据正确",item)
+        # print("数据解析",response.xpath('//img[@class="ui avatar image"]/@alt').getall())
+        # print("数据解析",response.xpath('//a[@class="imga"]/@href').getall())
+        
+        pass
+
+    def log(self, response):
+        pass
